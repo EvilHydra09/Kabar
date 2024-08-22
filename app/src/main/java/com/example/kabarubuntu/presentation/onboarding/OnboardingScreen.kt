c01e12447efa82e2b338b2755ade6f47f5186aec
Abhidnya Sonawane <abhidnyasonawane@Abhidnyas-Mac-mini.local>
2024-08-22 22:42:22 +0530

    Change the App Name

--- app/src/main/java/com/example/kabarubuntu/presentation/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/example/kabarubuntu/presentation/onboarding/OnboardingScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -96,7 +97,7 @@ fun OnBoardingScreen(
                     }, text = buttonState.value[1])
                 }
             }
-            Spacer(modifier = Modifier.weight(0.5f))
+            Spacer(modifier = Modifier.height(Dimens.Medium))
         }
     }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "KabarUbuntu"
+rootProject.name = "Kabar"
 include(":app")
  
